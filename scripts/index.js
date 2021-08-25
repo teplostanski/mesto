1d05c6d342ecede942c5d23311808c790a597ab6
fd7c6e1f <email@example.org>
2021-08-25 23:22:49 +0400

    вынес initial cards в отдельный файл

--- scripts/index.js
+++ scripts/index.js
@@ -17,44 +17,6 @@ const galleryCapture = document.querySelector('.popup__figcaption');
 const templateForCard = document.querySelector('#templateForCard');
 const cardsContainer = document.querySelector('.cards');
 
-const initialCards = [
-  {
-    name: 'Птичий мир',
-    link: './images/planet-bird-man.jpg',
-    alt: 'Птичий мир'
-  },
-
-  {
-    name: 'Флупилэнд',
-    link: './images/flupiland.jpg',
-    alt: 'Флупилэнд'
-  },
-
-  {
-    name: 'Планета с кричащим солнцем',
-    link: './images/sun.jpg',
-    alt: 'Планета с кричащим солнцем'
-  },
-
-  {
-    name: 'Планета Сквончи',
-    link: './images/sqwonch.jpg',
-    alt: 'Планета Сквончи'
-  },
-
-  {
-    name: 'Постапокалиптическая Земля',
-    link: './images/apocalips.jpg',
-    alt: 'Постапокалиптическая Земля'
-  },
-
-  {
-    name: 'Мир Юнити',
-    link: './images/unity.jpg',
-    alt: 'Мир Юнити'
-  },
-];
-
 function openPopup(popup) {
   popup.classList.add('popup_opened');
 }
---
+++ scripts/initial-cards.js
@@ -0,0 +1,37 @@
+const initialCards = [
+  {
+    name: 'Птичий мир',
+    link: './images/planet-bird-man.jpg',
+    alt: 'Птичий мир'
+  },
+
+  {
+    name: 'Флупилэнд',
+    link: './images/flupiland.jpg',
+    alt: 'Флупилэнд'
+  },
+
+  {
+    name: 'Планета с кричащим солнцем',
+    link: './images/sun.jpg',
+    alt: 'Планета с кричащим солнцем'
+  },
+
+  {
+    name: 'Планета Сквончи',
+    link: './images/sqwonch.jpg',
+    alt: 'Планета Сквончи'
+  },
+
+  {
+    name: 'Постапокалиптическая Земля',
+    link: './images/apocalips.jpg',
+    alt: 'Постапокалиптическая Земля'
+  },
+
+  {
+    name: 'Мир Юнити',
+    link: './images/unity.jpg',
+    alt: 'Мир Юнити'
+  },
+];
